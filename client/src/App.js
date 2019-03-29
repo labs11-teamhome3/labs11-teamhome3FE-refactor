d4cc235348a55f7e78c33cc64366a797cf2cb7cb
Clint Kunz <clintkunz@gmail.com>
2019-03-29 17:12:25 -0600

    auth0 functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+import Auth from './Auth/Auth.js'
+
 import './App.css';
 
 ////Components////
@@ -8,11 +10,15 @@ import DashboardView from './views/DashboardView';
 import TeamView from './views/TeamView';
 import LandingView from './views/LandingView';
 
+const auth = new Auth();
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={LandingView} />
+        <Route exact path='/'
+        render={props => <LandingView auth={auth} />} 
+        />
         <Route
           path="/dashboard"
           render={props => <DashboardView {...props} />}
