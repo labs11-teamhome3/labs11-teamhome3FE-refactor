cc0d2427e276b9fde4c99f2be3acde02d4940560
Clint Kunz <clintkunz@gmail.com>
2019-04-08 16:03:39 -0600

    can view ViewDocumentModal

--- client/src/components/TeamView/Tabs/DocumentTab/DocumentTab.jsx
+++ client/src/components/TeamView/Tabs/DocumentTab/DocumentTab.jsx
@@ -8,7 +8,7 @@ import { useMutation } from "../../../../graphQL/useMutation";
 /////Components/////
 import Document from "./Document";
 import CreateDocumentModal from "./CreateDocumentModal";
-//import ViewMessageModal from "./ViewMessageModal";
+import ViewDocumentModal from "./ViewDocumentModal";
 //import EditMessageModal from "./EditMessageModal";
 
 /////Queries/////
@@ -89,16 +89,16 @@ const DocumentTab = props => {
             toggleModal={toggleModal}
             setMsg={props.setMsg}
           />
-        ) : null}
+        ) : null}*/}
         {viewModalStatus ? (
-          <ViewMessageModal
+          <ViewDocumentModal
             modalStatus={viewModalStatus.status}
-            messageId={viewModalStatus.documentId}
+            documentId={viewModalStatus.documentId}
             toggleModal={toggleModal}
             teamId={props.teamId}
             setMsg={props.setMsg}
           />
-        ) : null} */}
+        ) : null} 
         </div>
     );
   };
