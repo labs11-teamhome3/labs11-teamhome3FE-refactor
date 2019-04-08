cbba806b90d7841c5f910043c2105420cbdd53bf
yannafaith <yannafaith97@gmail.com>
2019-04-08 14:13:38 -0700

    retrieves more info from user query

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -6,7 +6,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'manaje.auth0.com',
     clientID: 'yZSx2aTdImOQ8zWCqRZ5hhZJr6c7KXFz',
-    redirectUri: `${process.env.REACT_APP_REDIRECT_URL}`,
+    redirectUri: `http://localhost:3000`,
     responseType: 'token id_token',
     scope: 'openid profile email'
   });
