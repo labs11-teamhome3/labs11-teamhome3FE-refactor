ad4c1f870962558138520ceccd79fa762c75aa67
Clint Kunz <clintkunz@gmail.com>
2019-04-13 17:50:28 -0600

    drag to folder tooltip

--- client/src/components/TeamView/Tabs/DocumentTab/Document.jsx
+++ client/src/components/TeamView/Tabs/DocumentTab/Document.jsx
@@ -5,6 +5,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import File from '@material-ui/icons/InsertDriveFileOutlined';
 import MoreHoriz from '@material-ui/icons/MoreHoriz';
+import Tooltip from '@material-ui/core/Tooltip';
 import moment from 'moment';
 
 const styles = theme => ({
@@ -24,7 +25,9 @@ const Document = props => {
     <TableRow 
       ref={instance => connectDragSource(ReactDOM.findDOMNode(instance))} 
     >
+    <Tooltip title="Drag to folder">
       <TableCell><File/>{props.document.title}</TableCell>
+    </Tooltip>
       <TableCell>{moment(props.document.createdAt).calendar()}</TableCell>
       <TableCell>{props.document.user.name}</TableCell>
       <TableCell><a style={{textDecoration:"none", color:"inherit"}} href={props.document.doc_url} target='_blank'>{props.document.doc_url}</a></TableCell>
