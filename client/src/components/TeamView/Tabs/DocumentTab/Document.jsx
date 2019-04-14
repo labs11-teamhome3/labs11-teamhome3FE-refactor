6f07084f6da967687b155599ae5dbc0bbc81114f
Clint Kunz <clintkunz@gmail.com>
2019-04-13 22:07:56 -0600

    positioned tooltip below file icon

--- client/src/components/TeamView/Tabs/DocumentTab/Document.jsx
+++ client/src/components/TeamView/Tabs/DocumentTab/Document.jsx
@@ -6,6 +6,7 @@ import TableRow from '@material-ui/core/TableRow';
 import File from '@material-ui/icons/InsertDriveFileOutlined';
 import MoreHoriz from '@material-ui/icons/MoreHoriz';
 import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
 import moment from 'moment';
 
 const styles = theme => ({
@@ -25,9 +26,12 @@ const Document = props => {
     <TableRow 
       ref={instance => connectDragSource(ReactDOM.findDOMNode(instance))} 
     >
-    <Tooltip title="Drag to folder">
-      <TableCell><File/>{props.document.title}</TableCell>
-    </Tooltip>
+      <TableCell>
+        <Tooltip title="Drag to folder" TransitionComponent={Zoom}>
+          <File/>
+        </Tooltip>
+          {props.document.title}
+      </TableCell>
       <TableCell>{moment(props.document.createdAt).calendar()}</TableCell>
       <TableCell>{props.document.user.name}</TableCell>
       <TableCell><a style={{textDecoration:"none", color:"inherit"}} href={props.document.doc_url} target='_blank'>{props.document.doc_url}</a></TableCell>
