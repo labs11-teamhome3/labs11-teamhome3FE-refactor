618cb4ccb4d80fd935db78d51f271a0feac6552e
Moonkaman <nickbatmanglidj@gmail.com>
2019-04-16 09:55:56 -0600

    Styling work on todos tab

--- client/src/components/TeamView/Tabs/TodosTab/Todo.jsx
+++ client/src/components/TeamView/Tabs/TodosTab/Todo.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Divider from "@material-ui/core/Divider";
 import { useMutation } from "../../../../graphQL/useMutation";
 import gql from "graphql-tag";
+import Checkbox from "@material-ui/core/Checkbox";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
 
 // import { TODO_LIST_QUERY } from "../../../../graphQL/Queries";
 
@@ -17,7 +23,18 @@ const TOGGLE_COMPLETE = gql`
   }
 `;
 
+const styles = theme => ({
+  todoCont: {
+    width: "100%"
+  },
+  listItem: {
+    width: "100%"
+  }
+});
+
 const Todo = props => {
+  const { classes } = props;
+  const [todoComplete, setTodoComplete] = useState(props.todo.completed);
   const [toggleComplete] = useMutation(TOGGLE_COMPLETE, {
     // update: (cache, { data }) => {
     //   const { todoList } = cache.readQuery({
@@ -44,22 +61,41 @@ const Todo = props => {
     variables: {
       todoId: props.todo.id
     },
-    onCompleted: e => {},
+    onCompleted: e => {
+      if (e.toggleTodoComplete.completed) {
+        props.setCompleteTodos(props.completeTodos + 1);
+      } else {
+        props.setCompleteTodos(props.completeTodos - 1);
+      }
+    },
     onError: err => console.log(err)
   });
+
+  const listToggleComplete = _ => {
+    toggleComplete();
+    setTodoComplete(!todoComplete);
+  };
   return (
-    <div className="todo">
-      <input
-        type="checkbox"
-        checked={props.todo.completed}
-        onClick={toggleComplete}
-        // onChange gets rid of a console warning
-        onChange={() => {}}
-      />
-      <h3>{props.todo.description}</h3>
+    <div className={classes.todoCont}>
+      <ListItem
+        className={classes.listItem}
+        onClick={listToggleComplete}
+        button
+      >
+        <ListItemText primary={props.todo.description} />
+        <ListItemSecondaryAction>
+          <Checkbox
+            checked={todoComplete}
+            onChange={_ => setTodoComplete(!todoComplete)}
+            onClick={toggleComplete}
+            value="checkedB"
+            color="primary"
+          />
+        </ListItemSecondaryAction>
+      </ListItem>
       <Divider />
     </div>
   );
 };
 
-export default Todo;
+export default withStyles(styles)(Todo);
